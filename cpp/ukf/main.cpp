7c609ae6c139cd1ad6e151e68065dfb175f16d76
daniele-bruno <daniele.bruno@fcagroup.com>
2021-04-14 23:07:28 +0200

    generate augmented sigma points

--- cpp/ukf/main.cpp
+++ cpp/ukf/main.cpp
@@ -9,14 +9,17 @@ int main() {
   // Create a UKF instance
   UKF ukf;
 
-  /**
-   * Programming assignment calls
-   */
+  // sigma points
   MatrixXd Xsig = MatrixXd(5, 11);
   ukf.GenerateSigmaPoints(&Xsig);
-
   // print result
   std::cout << "Xsig = " << std::endl << Xsig << std::endl;
 
+  // augmentedsigma points 
+  MatrixXd Xsig_aug = MatrixXd(7, 15);
+  ukf.AugmentedSigmaPoints(&Xsig_aug);
+  // print result
+  std::cout << "Xsig_aug = " << std::endl << Xsig_aug << std::endl;
+  
   return 0;
 }
